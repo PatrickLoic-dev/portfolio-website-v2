ba3fec0a159636c1725e27b9ae609ce422065ba5
Patrick Loic <kangueloic9@gmail.com>
2025-07-18 16:04:48 +0100

    feat: Add structured data scripts for SEO and improve code formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,15 +12,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  let isVisible = false; 
+  let isVisible = false;
   let isFooterVisible = false;
   const pathname = usePathname();
 
-  if ( pathname === "/about" || pathname === "/projects" ) {
+  if (pathname === "/about" || pathname === "/projects") {
     isVisible = true;
   }
 
-  if ( pathname === "/contact" ) {
+  if (pathname === "/contact") {
     isFooterVisible = true;
   }
 
@@ -29,6 +29,54 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: `
+        {
+          "@context": "https://schema.org",
+          "@type": "WebSite",
+          "url": "https://www.kangueloic.me",
+          "name": "Kangue Loïc Portfolio",
+          "description": "Showcasing the journey, skills, and projects of Kangue Loïc, a creative developer passionate about crafting unique digital experiences.",
+          "potentialAction": {
+            "@type": "SearchAction",
+            "target": "https://www.kangueloic.me/search?q={search_term_string}",
+            "query-input": "required name=search_term_string"
+          }
+        }
+        ` }} />
+        <script type="application/ld+json" dangerouslySetInnerHTML={{
+          __html: `
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement": [
+            {
+              "@type": "ListItem",
+              "position": 1,
+              "name": "Home",
+              "item": "https://www.kangueloic.me"
+            },
+            {
+              "@type": "ListItem",
+              "position": 2,
+              "name": "About",
+              "item": "https://www.kangueloic.me/about"
+            },
+            {
+              "@type": "ListItem",
+              "position": 3,
+              "name": "Projects",
+              "item": "https://www.kangueloic.me/projects"
+            },
+            {
+              "@type": "ListItem",
+              "position": 4,
+              "name": "Contact",
+              "item": "https://www.kangueloic.me/contact"
+            }
+          ]
+        }
+        ` }} />
       </head>
       <body className="w-screen h-[100vh] sm:w-[100%] md:h-[100vh] overflow-x-hidden text-white">
         <Header />
