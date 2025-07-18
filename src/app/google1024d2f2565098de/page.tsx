78a97455a046a65f4be52c67fc79d16ea914b2e3
Patrick Loic <kangueloic9@gmail.com>
2025-07-18 16:21:46 +0100

    feat: Reintroduce Google site verification meta tag in new page component

--- src/app/google1024d2f2565098de/page.tsx
+++ src/app/google1024d2f2565098de/page.tsx
@@ -1,5 +1,10 @@
 function google1024d2f2565098de (){
     return (
-            <meta name="google-site-verification" content="google1024d2f2565098de.html" />
+            <>
+                <meta name="google-site-verification" content="google1024d2f2565098de.html" />
+            </>
+            
     )
 }
+
+export default google1024d2f2565098de;
